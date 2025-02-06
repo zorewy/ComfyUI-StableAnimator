e0d0cc44107cf12b061e91bb4d6c21ff007dde77
zore <624110778@qq.com>
2025-02-06 10:29:53 +0800

    feat: init

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ NODE_CLASS_MAPPINGS = {
     "StableAnimatorNode": StableAnimatorNode
 }
 
-NODE_DISPLAY_NAMES_MAPPINGS = {
+NODE_DISPLAY_NAME_MAPPINGS = {
     "StableAnimatorNode": "StableAnimator"
 }
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
